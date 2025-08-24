c2be2935ee38ff2711a98f8882fc582f732de9f5
somsakulbananas <somsakulbananas@gmail.com>
2025-08-24 23:46:55 +0700

    HW Value Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -3,3 +3,12 @@
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+let Name = "Karter"
+let Lname = "Anderson"
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const nameShop = "Chef's Table by Chef James"
+const priceBill = 15000
+const countShopOpen = 1500
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+priceBill = 5000
+countShopOpen = 2500
